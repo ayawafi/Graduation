c5e7ec8f8d6f78d40d7f0474fd920a49b3b412bf
Aya Wafi <ayawafi.2534@gmail.com>
2023-05-22 11:45:26 +0300

    Newupdates

--- Clinic_Core/Managers/Services/BlogManager.cs
+++ Clinic_Core/Managers/Services/BlogManager.cs
@@ -1,4 +1,6 @@
-﻿using Clinic_Core.Managers.Interfaces;
+﻿using AutoMapper;
+using Clinic_Core.Managers.Interfaces;
+using Clinic_DbModel.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,13 @@ namespace Clinic_Core.Managers
 {
     public class BlogManager : IBlogManager
     {
+        private clinic_dbContext _dbContext;
+        private IMapper _mapper;
+
+        public BlogManager(clinic_dbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
     }
 }
